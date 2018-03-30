ffd1a7fca5a0675156e7376a6283e786dbd9ed37
Mark Kahn <m-github@vouchr.com>
2018-03-30 23:22:30 +0800

    Adding prompts for next episode in a series & restructuring

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// @flow
-
 const fs = require('fs');
-const Telnet = require('telnet-client');
-const trash = require('trash');
+const path = require('path');
 const childProcess = require('child_process');
 
-const trashedFiles = {};
-const skippedFiles = {};
+const Telnet = require('telnet-client');
+const trash = require('trash');
+const notifier = require('node-notifier');
 
 const CLEAN_CWD = '/Users/mark/Downloads/Videos/';
 const CLEAN_SH = '/Users/mark/Downloads/Videos/clean.sh';
@@ -19,100 +17,176 @@ const INTROS = [
   },
 ];
 
-function connect() {
-  return new Promise((resolve, reject) => {
-    const params = {
-      host: '127.0.0.1',
-      port: 4212,
-      shellPrompt: '> ',
-      timeout: 500,
-      execTimeout: 500,
-      sendTimeout: 500,
-      password: 'password',
-      passwordPrompt: /Password:.*/,
-      debug: true,
-      echoLines: 0,
-      ors: '\r\n',
-    };
+const POLL_INTERVAL = 1500;
+const SKIP_INTRO_MAX_TIME = 10;
+const PLAY_COMPLETE_PERCENT = 0.9;
+const CONNECTION_PARAMS = {
+  host: '127.0.0.1',
+  port: 4212,
+  shellPrompt: '> ',
+  timeout: 500,
+  execTimeout: 500,
+  sendTimeout: 500,
+  password: 'password',
+  passwordPrompt: /Password:.*/,
+  debug: true,
+  echoLines: 0,
+  ors: '\r\n',
+};
 
-    const connection = new Telnet();
+class ConnectionMonitor {
+  constructor(connection) {
+    console.log('connection opened');
+    this.trashedFiles = {};
+    this.skippedFiles = {};
+    this.fileToTrash = '';
+    this.connection = connection;
+    this.pollInterval = setInterval(() => this.pollStatus(), POLL_INTERVAL);
+    this.playing = false;
+    this.lastTime = 0;
+    this.lastFileName = '';
 
-    connection
-      .on('error', reject)
-      .connect(params)
-      .catch(reject)
-      .then(() => resolve(connection));
-  });
-}
+    connection.on('close', () => {
+      console.log('connection closed');
+      this.stopped();
+      clearInterval(this.pollInterval);
+      pollForConnection();
+    });
+  }
 
-async function monitor(connection) {
-  console.log('connection opened');
-
-  const intervals = [];
-
-  connection.oldExec = connection.exec;
-  connection.exec = async cmd => {
-    const response = await connection.oldExec(cmd);
+  async exec(cmd) {
+    const response = await this.connection.exec(cmd);
     return response.trim();
-  };
+  }
 
-  async function pollStatus() {
-    const time = +await connection.exec('get_time');
-    const length = +await connection.exec('get_length');
+  async pollStatus() {
+    const time = +await this.exec('get_time');
+    const length = +await this.exec('get_length');
+    const fileName = await this.genFileName();
+    this.trackTime(fileName, time);
 
-    if (time < 10) {
-      skipIntro();
-    } else if (time >= length * 0.9) {
-      trashCurrentFile();
+    if (time < SKIP_INTRO_MAX_TIME) {
+      this.skipIntro();
+    } else if (time >= length * PLAY_COMPLETE_PERCENT) {
+      this.trashCurrentFile();
     }
   }
 
-  async function genFileName() {
-    const status = await connection.exec('status');
+  trackTime(fileName, time) {
+    const playing = time !== this.lastTime;
+    if (this.lastFileName) {
+      if (fileName !== this.lastFileName || !playing) {
+        this.stopped();
+      }
+    }
+
+    this.lastFileName = fileName;
+    this.lastTime = time;
+    if (playing !== this.playing) {
+      this.playing = playing;
+    }
+  }
+
+  async genFileName() {
+    const status = await this.exec('status');
     const fileName = /input: file:\/\/(\/.*?) \)/.exec(status)[1];
 
     return fileName;
   }
 
-  async function skipIntro() {
-    const fileName = await genFileName();
+  async skipIntro() {
+    const fileName = await this.genFileName();
 
     INTROS.forEach(intro => {
-      if (skippedFiles[fileName]) return;
+      if (this.skippedFiles[fileName]) return;
+
       if (intro.fileRx.test(fileName)) {
         const timeToSkip = intro.length;
-
-        connection.exec(`seek ${timeToSkip}`);
-
-        skippedFiles[fileName] = 1;
+        this.exec(`seek ${timeToSkip}`);
+        this.skippedFiles[fileName] = 1;
       }
     });
   }
 
-  async function trashCurrentFile() {
-    const fileName = await genFileName();
+  async stopped() {
+    const fileName = this.fileToTrash;
+    if (!fileName) return;
 
-    if (!PATHS_TO_TRASH.test(fileName) || trashedFiles[fileName]) return;
+    // trash(fileName);
+    console.log(`trash ${fileName}`);
 
-    trash(fileName);
-    trashedFiles[fileName] = 1;
-
+    const fileParsed = path.parse(fileName);
     childProcess.exec(CLEAN_SH, { cwd: CLEAN_CWD }, () => {});
+
+    // Showing 2 notifications makes the first one persist no matter what, so commenting out for now
+    // notifier.notify({
+    //   title: 'Video Complete',
+    //   message: `Deleted ${fileParsed.base}`,
+    //   timeout: 2,
+    // });
+    setTimeout(() => this.promptNextFile(fileParsed), 1000);
+
+    this.fileToTrash = '';
   }
 
-  intervals.push(setInterval(pollStatus, 1000));
+  async promptNextFile(fileParsed) {
+    const files = fs.readdirSync(fileParsed.dir);
+    files.sort((a, b) => a > b);
 
-  connection.on('close', () => {
-    console.log('connection closed');
-    pollForConnection();
-    intervals.forEach(i => clearInterval(i));
+    const nextFile = files[files.indexOf(fileParsed.base) + 1];
+    const isSameSeries = this.isSameSeries(fileParsed.base, nextFile);
+
+    if (!isSameSeries) return;
+
+    notifier.notify(
+      {
+        title: 'Play Next Episode?',
+        message: `Play ${nextFile}?`,
+        closeLabel: 'No',
+        actions: 'Play',
+        timeout: 30,
+      },
+      (err, response, metadata) => {
+        if (response === 'activate' && metadata.activationValue === 'Play') {
+          const nextFilePath = path.join(fileParsed.dir, nextFile);
+          this.exec(`add ${nextFilePath}`);
+        }
+      },
+    );
+  }
+
+  isSameSeries(fileA, fileB) {
+    if (!fileA || !fileB) return false;
+
+    return /^[^\d]+/.exec(fileA)[0] === /^[^\d]+/.exec(fileB)[0];
+  }
+
+  async trashCurrentFile() {
+    const fileName = await this.genFileName();
+
+    if (!PATHS_TO_TRASH.test(fileName) || this.trashedFiles[fileName]) return;
+
+    this.fileToTrash = fileName;
+    this.trashedFiles[fileName] = 1;
+  }
+}
+
+function connect() {
+  return new Promise((resolve, reject) => {
+    const connection = new Telnet();
+
+    connection
+      .on('error', reject)
+      .connect(CONNECTION_PARAMS)
+      .then(() => resolve(connection))
+      .catch(reject);
   });
 }
 
 function pollForConnection() {
   connect()
     .then(connection => {
-      monitor(connection);
+      new ConnectionMonitor(connection);
     })
     .catch(() => setTimeout(pollForConnection, 5000));
 }
